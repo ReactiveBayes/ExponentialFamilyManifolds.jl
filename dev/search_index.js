b9367dc3775ae025c20eed1fcbf0842e3f8b2022
Documenter.jl <documenter@juliadocs.github.io>
2024-06-10 11:33:20 +0000

    build based on ee6e31e

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = ExponentialFamilyManifolds","category":"page"},{"location":"#ExponentialFamilyManifolds","page":"Home","title":"ExponentialFamilyManifolds","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for ExponentialFamilyManifolds.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [ExponentialFamilyManifolds]","category":"page"}]
+}
