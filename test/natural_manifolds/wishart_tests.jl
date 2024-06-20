b078caade6383be748d732298add93a6eee7f8d8
İsmail Şenöz <ismailsenoz@Ismails-MacBook-Pro.local>
2024-06-20 20:28:20 +0200

    add wishart manifold

---
+++ test/natural_manifolds/wishart_tests.jl
@@ -0,0 +1,10 @@
+@testitem "Check `Wishart` natural manifold" begin
+    include("natural_manifolds_setuptests.jl")
+    import ExponentialFamily: WishartFast
+    test_natural_manifold() do rng
+        k = rand(rng, 2:10)
+        L = LowerTriangular(randn(rng, k, k))
+        C = L * L' + k * I
+        return WishartFast(k+2, C)
+    end
+end
