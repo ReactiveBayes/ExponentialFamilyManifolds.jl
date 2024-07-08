81e1a30439f1742525e64dce118e6e9ea43cfeef
İsmail Şenöz <ismailsenoz@Ismails-MacBook-Pro.local>
2024-07-08 14:10:52 +0300

    change rand range for binomial distributions

--- test/natural_manifolds/binomial_tests.jl
+++ test/natural_manifolds/binomial_tests.jl
@@ -2,6 +2,6 @@
     include("natural_manifolds_setuptests.jl")
 
     test_natural_manifold() do rng
-        return Binomial(rand(rng, 1:20), rand(rng, 0:1))
+        return Binomial(rand(rng, 1:20), rand(rng, 0:0.001:1))
     end
 end
