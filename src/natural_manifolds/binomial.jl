4a1884fdc013694f55a290a7ec66597298815746
İsmail Şenöz <ismailsenoz@Ismails-MacBook-Pro.local>
2024-07-08 11:49:09 +0300

    check for positive conditioner

--- src/natural_manifolds/binomial.jl
+++ src/natural_manifolds/binomial.jl
@@ -4,6 +4,7 @@
 Get the natural manifold base for the `Binomial` distribution.
 """
 function get_natural_manifold_base(::Type{Binomial}, ::Tuple{}, conditioner=nothing)
+    @assert conditioner > 0 "Conditioner $(conditioner) should be positive"
     return Euclidean(1)
 end
 
@@ -13,5 +14,6 @@ end
 Converts the `point` to a compatible representation for the natural manifold of type `Binomial`.
 """
 function partition_point(::Type{Binomial}, ::Tuple{}, p, conditioner=nothing)
+    @assert conditioner > 0 "Conditioner $(conditioner) should be positive"
     return p
 end
