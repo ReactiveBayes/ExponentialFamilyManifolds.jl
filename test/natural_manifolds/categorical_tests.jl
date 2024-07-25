48bcae1d5c842ce174408298085f5f294c3b68b0
Bagaev Dmitry <bvdmitri@gmail.com>
2024-07-25 18:36:19 +0200

    add test

--- test/natural_manifolds/categorical_tests.jl
+++ test/natural_manifolds/categorical_tests.jl
@@ -1,6 +1,6 @@
 @testitem "Check `Categorical` natural manifold" begin
     include("natural_manifolds_setuptests.jl")
-    
+
     test_natural_manifold() do rng
         p = rand(rng, 10)
         normalize!(p, 1)
@@ -37,4 +37,22 @@ end
     q = gradient_descent(M, f, g, rand(rng, M))
     @test q ∈ M
     @test mean(q) ≈ 0.5 atol = 1e-1
+end
+
+@testitem "Check that the manifold for `Categorical` interacts nicely with `ForwardDiff`" begin
+    include("natural_manifolds_setuptests.jl")
+
+    using ForwardDiff
+    # JuliaDiff/ForwardDiff.jl#706
+    rng = StableRNG(42)
+    p = rand(rng, 10)
+    normalize!(p, 1)
+    distribution = Categorical(p)
+    sample = rand(rng, distribution)
+    dims = size(sample)
+    ef = convert(ExponentialFamilyDistribution, distribution)
+    T = ExponentialFamily.exponential_family_typetag(ef)
+    M = get_natural_manifold(T, dims, getconditioner(ef))
+
+    @test all(≈(1), ForwardDiff.gradient(sum, rand(rng, M)))
 end
