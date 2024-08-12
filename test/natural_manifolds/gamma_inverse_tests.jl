a7ef874530cbda6a14b2db00620e296bd62d70f6
Mykola Lukashchuk <nikola.lukashuk@gmail.com>
2024-08-12 16:16:10 +0200

    fix: use InverseGamma from ExponentialFamily

--- test/natural_manifolds/gamma_inverse_tests.jl
+++ test/natural_manifolds/gamma_inverse_tests.jl
@@ -2,6 +2,6 @@
     include("natural_manifolds_setuptests.jl")
 
     test_natural_manifold() do rng
-        return InverseGamma(10rand(rng), 10rand(rng))
+        return ExponentialFamily.InverseGamma(10rand(rng), 10rand(rng))
     end
 end
