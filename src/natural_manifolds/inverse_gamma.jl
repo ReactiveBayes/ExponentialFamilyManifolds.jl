a2c6dedc87ec7f890848e2c580a7cae184f2d031
Mykola Lukashchuk <nikola.lukashuk@gmail.com>
2024-08-12 16:25:51 +0200

    docs: specify type

--- src/natural_manifolds/inverse_gamma.jl
+++ src/natural_manifolds/inverse_gamma.jl
@@ -1,7 +1,7 @@
 """
-    get_natural_manifold_base(::Type{GammaInverse}, ::Tuple{}, conditioner=nothing)
+    get_natural_manifold_base(::Type{ExponentialFamily.GammaInverse}, ::Tuple{}, conditioner=nothing)
 
-Get the natural manifold base for the `GammaInverse` distribution.
+Get the natural manifold base for the `ExponentialFamily.GammaInverse` distribution.
 """
 function get_natural_manifold_base(::Type{ExponentialFamily.GammaInverse}, ::Tuple{}, conditioner=nothing)
     return ProductManifold(
@@ -10,9 +10,9 @@ function get_natural_manifold_base(::Type{ExponentialFamily.GammaInverse}, ::Tup
 end
 
 """
-    partition_point(::Type{GammaInverse}, ::Tuple{}, p, conditioner=nothing)
+    partition_point(::Type{ExponentialFamily.GammaInverse}, ::Tuple{}, p, conditioner=nothing)
 
-Converts the `point` to a compatible representation for the natural manifold of type `GammaInverse`.
+Converts the `point` to a compatible representation for the natural manifold of type `ExponentialFamily.GammaInverse`.
 """
 function partition_point(::Type{ExponentialFamily.GammaInverse}, ::Tuple{}, p, conditioner=nothing)
     return ArrayPartition(view(p, 1:1), view(p, 2:2))
