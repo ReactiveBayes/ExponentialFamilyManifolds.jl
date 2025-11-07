866b544906d483b22f414f6f3b16a37310ec1ab7
Tim <t.n.nisslbeck@tue.nl>
2025-11-07 06:52:22 +0100

    Makefile: Add make test. #45 add "make test test_args=..." filter. make format

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,19 +1,50 @@
-using Aqua, Hwloc, ExponentialFamilyManifolds, Test, ReTestItems
+using Aqua, Hwloc, ExponentialFamilyManifolds, ReTestItems
 
-Aqua.test_all(
-    ExponentialFamilyManifolds;
-    ambiguities=false,
-    deps_compat=(; check_extras=false, check_weakdeps=true),
-    piracies=false,
-)
+if get(ENV, "RUN_AQUA", "true") == "true"
+    Aqua.test_all(
+        ExponentialFamilyManifolds;
+        ambiguities=false,
+        deps_compat=(; check_extras=false, check_weakdeps=true),
+        piracies=false,
+    )
+end
 
-ncores = max(Hwloc.num_physical_cores(), 1)
-nthreads = max(Hwloc.num_virtual_cores(), 1)
-threads_per_core = max(Int(floor(nthreads / ncores)), 1)
+nthreads, ncores = Hwloc.num_virtual_cores(), Hwloc.num_physical_cores()
+nthreads, ncores = max(nthreads, 1), max(ncores, 1)
+nworker_threads = Int(nthreads / ncores)
+memory_threshold = 1.0
 
-runtests(
-    ExponentialFamilyManifolds;
-    nworkers=ncores,
-    nworker_threads=threads_per_core,
-    memory_threshold=1.0,
-)
+pkg_root = dirname(dirname(pathof(ExponentialFamilyManifolds)))
+test_root = joinpath(pkg_root, "test")
+
+if isempty(ARGS)
+    runtests(
+        ExponentialFamilyManifolds;
+        nworkers=ncores,
+        nworker_threads=nworker_threads,
+        memory_threshold=memory_threshold,
+    )
+else
+    for arg in ARGS
+        # Translate colon syntax (e.g., rules:normal_mean_variance → rules/normal_mean_variance)
+        candidate = join(split(arg, ":"), "/")
+
+        # Build possible test paths relative to the package test directory
+        paths = [joinpath(test_root, candidate), joinpath(test_root, candidate * ".jl")]
+
+        path = findfirst(ispath, paths)
+
+        if path !== nothing
+            selected_path = paths[path]
+            @info "Running selective tests from $selected_path"
+            runtests(
+                selected_path;
+                nworkers=ncores,
+                nworker_threads=nworker_threads,
+                memory_threshold=memory_threshold,
+            )
+        else
+            @warn "Test target not found: $arg"
+        end
+    end
+end
