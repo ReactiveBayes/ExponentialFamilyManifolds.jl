6bb4b922dbcccadb4d3cc67832b10d17cffc12c4
Mykola Lukashchuk <nikola.lukashuk@gmail.com>
2025-11-19 16:22:26 +0100

    fix: clean bernoulli

--- src/metric_manifolds/bernoulli.jl
+++ src/metric_manifolds/bernoulli.jl
@@ -1,3 +1,95 @@
+using ManifoldsBase
+
+# Stop forwarding for functions we implement
+@inline ManifoldsBase.get_forwarding_type(
+    ::WithMetric{F,Bernoulli,NaturalMetric}, ::typeof(ManifoldsBase.exp), ::Type
+) where {F} = _STOP
+@inline ManifoldsBase.get_forwarding_type(
+    ::WithMetric{F,Bernoulli,NaturalMetric}, ::typeof(ManifoldsBase.exp!), ::Type
+) where {F} = _STOP
+@inline ManifoldsBase.get_forwarding_type(
+    ::WithMetric{F,Bernoulli,NaturalMetric}, ::typeof(ManifoldsBase.exp_fused), ::Type
+) where {F} = _STOP
+@inline ManifoldsBase.get_forwarding_type(
+    ::WithMetric{F,Bernoulli,NaturalMetric}, ::typeof(ManifoldsBase.exp_fused!), ::Type
+) where {F} = _STOP
+@inline ManifoldsBase.get_forwarding_type(
+    ::WithMetric{F,Bernoulli,NaturalMetric}, ::typeof(ManifoldsBase.log), ::Type
+) where {F} = _STOP
+@inline ManifoldsBase.get_forwarding_type(
+    ::WithMetric{F,Bernoulli,NaturalMetric}, ::typeof(ManifoldsBase.log!), ::Type
+) where {F} = _STOP
+@inline ManifoldsBase.get_forwarding_type(
+    ::WithMetric{F,Bernoulli,NaturalMetric},
+    ::typeof(ManifoldsBase.parallel_transport_to),
+    ::Type,
+) where {F} = _STOP
+@inline ManifoldsBase.get_forwarding_type(
+    ::WithMetric{F,Bernoulli,NaturalMetric},
+    ::typeof(ManifoldsBase.parallel_transport_to!),
+    ::Type,
+) where {F} = _STOP
+@inline ManifoldsBase.get_forwarding_type(
+    ::WithMetric{F,Bernoulli,NaturalMetric},
+    ::typeof(ManifoldsBase.parallel_transport_direction),
+    ::Type,
+) where {F} = _STOP
+@inline ManifoldsBase.get_forwarding_type(
+    ::WithMetric{F,Bernoulli,NaturalMetric},
+    ::typeof(ManifoldsBase.parallel_transport_direction!),
+    ::Type,
+) where {F} = _STOP
+@inline ManifoldsBase.get_forwarding_type(
+    ::WithMetric{F,Bernoulli,NaturalMetric},
+    ::typeof(ManifoldsBase.vector_transport_to),
+    ::Type,
+) where {F} = _STOP
+@inline ManifoldsBase.get_forwarding_type(
+    ::WithMetric{F,Bernoulli,NaturalMetric},
+    ::typeof(ManifoldsBase.vector_transport_to!),
+    ::Type,
+) where {F} = _STOP
+
+# Also catch the single-argument versions (without Type)
+@inline ManifoldsBase.get_forwarding_type(
+    ::WithMetric{F,Bernoulli,NaturalMetric}, ::typeof(ManifoldsBase.exp)
+) where {F} = _STOP
+@inline ManifoldsBase.get_forwarding_type(
+    ::WithMetric{F,Bernoulli,NaturalMetric}, ::typeof(ManifoldsBase.exp!)
+) where {F} = _STOP
+@inline ManifoldsBase.get_forwarding_type(
+    ::WithMetric{F,Bernoulli,NaturalMetric}, ::typeof(ManifoldsBase.exp_fused)
+) where {F} = _STOP
+@inline ManifoldsBase.get_forwarding_type(
+    ::WithMetric{F,Bernoulli,NaturalMetric}, ::typeof(ManifoldsBase.exp_fused!)
+) where {F} = _STOP
+@inline ManifoldsBase.get_forwarding_type(
+    ::WithMetric{F,Bernoulli,NaturalMetric}, ::typeof(ManifoldsBase.log)
+) where {F} = _STOP
+@inline ManifoldsBase.get_forwarding_type(
+    ::WithMetric{F,Bernoulli,NaturalMetric}, ::typeof(ManifoldsBase.log!)
+) where {F} = _STOP
+@inline ManifoldsBase.get_forwarding_type(
+    ::WithMetric{F,Bernoulli,NaturalMetric}, ::typeof(ManifoldsBase.parallel_transport_to)
+) where {F} = _STOP
+@inline ManifoldsBase.get_forwarding_type(
+    ::WithMetric{F,Bernoulli,NaturalMetric}, ::typeof(ManifoldsBase.parallel_transport_to!)
+) where {F} = _STOP
+@inline ManifoldsBase.get_forwarding_type(
+    ::WithMetric{F,Bernoulli,NaturalMetric},
+    ::typeof(ManifoldsBase.parallel_transport_direction),
+) where {F} = _STOP
+@inline ManifoldsBase.get_forwarding_type(
+    ::WithMetric{F,Bernoulli,NaturalMetric},
+    ::typeof(ManifoldsBase.parallel_transport_direction!),
+) where {F} = _STOP
+@inline ManifoldsBase.get_forwarding_type(
+    ::WithMetric{F,Bernoulli,NaturalMetric}, ::typeof(ManifoldsBase.vector_transport_to)
+) where {F} = _STOP
+@inline ManifoldsBase.get_forwarding_type(
+    ::WithMetric{F,Bernoulli,NaturalMetric}, ::typeof(ManifoldsBase.vector_transport_to!)
+) where {F} = _STOP
+
 function _geodesic_exact_bernoulli(t, x0, v0)
     # Compute metric at initial point
     g0 = exp(x0) / (1 + exp(x0))^2
@@ -38,40 +130,17 @@ function _log_map_bernoulli(x0, x1)
     return v0
 end
 
-# Exponential map implementations - these override the default forwarding behavior
-function ManifoldsBase.exp!(::WithMetric{F,Bernoulli,NaturalMetric}, q, p, X) where {F}
-    x, _ = _geodesic_exact_bernoulli(1, p[1], X[1])
-    q .= x
-    return q
-end
-
-function ManifoldsBase.exp(::WithMetric{F,Bernoulli,NaturalMetric}, p, X) where {F}
-    x, _ = _geodesic_exact_bernoulli(1, p[1], X[1])
-    return [x]
-end
-
 function ManifoldsBase.exp_fused!(
     ::WithMetric{F,Bernoulli,NaturalMetric}, q, p, X, t::Number
 ) where {F}
-    x, _ = _geodesic_exact_bernoulli(1, p[1], X[1])
+    x, _ = _geodesic_exact_bernoulli(t, p[1], X[1])
     q .= x
     return q
-end
-
-function ManifoldsBase.exp_fused(
-    ::WithMetric{F,Bernoulli,NaturalMetric}, p, X, t::Number
-) where {F}
-    x, _ = _geodesic_exact_bernoulli(t, p[1], X[1])
-    return [x]
 end
 
 function ManifoldsBase.log!(::WithMetric{F,Bernoulli,NaturalMetric}, X, p, q) where {F}
     X .= _log_map_bernoulli(p[1], q[1])
     return X
-end
-
-function ManifoldsBase.log(::WithMetric{F,Bernoulli,NaturalMetric}, p, q) where {F}
-    return _log_map_bernoulli(p[1], q[1])
 end
 
 _sqrtg_eta(η) = 0.5 .* sech.(η ./ 2)
@@ -81,43 +150,12 @@ function ManifoldsBase.parallel_transport_to(
 ) where {F}
     s_p = _sqrtg_eta(p)
     s_q = _sqrtg_eta(q)
-    X .* (s_p ./ s_q)
+    return X .* (s_p ./ s_q)
 end
 
 function ManifoldsBase.parallel_transport_to!(
-    ::WithMetric{F,Bernoulli,NaturalMetric}, Y, p, X, q; kwargs...
+    M::WithMetric{F,Bernoulli,NaturalMetric}, Y, p, X, q; kwargs...
 ) where {F}
-    # error("Call here")
-    s_p = _sqrtg_eta(p)
-    s_q = _sqrtg_eta(q)
-    @. Y = X * (s_p / s_q)
-    Y
-end
-
-# Parallel transport in a direction
-function ManifoldsBase.parallel_transport_direction(
-    M::WithMetric{F,Bernoulli,NaturalMetric}, p, X, d; kwargs...
-) where {F}
-    q = exp(M, p, d)
-    return parallel_transport_to(M, p, X, q)
-end
-
-function ManifoldsBase.parallel_transport_direction!(
-    M::WithMetric{F,Bernoulli,NaturalMetric}, Y, p, X, d; kwargs...
-) where {F}
-    q = exp(M, p, d)
-    return parallel_transport_to!(M, Y, p, X, q)
-end
-
-# Vector transport with ParallelTransport method - this is what check_geodesic uses
-function ManifoldsBase.vector_transport_to(
-    M::WithMetric{F,Bernoulli,NaturalMetric}, p, X, q, ::ManifoldsBase.ParallelTransport
-) where {F}
-    return parallel_transport_to(M, p, X, q)
-end
-
-function ManifoldsBase.vector_transport_to!(
-    M::WithMetric{F,Bernoulli,NaturalMetric}, Y, p, X, q, ::ManifoldsBase.ParallelTransport
-) where {F}
-    return parallel_transport_to!(M, Y, p, X, q)
+    Y .= ManifoldsBase.parallel_transport_to(M, p, X, q; kwargs...)
+    return Y
 end
